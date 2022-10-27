6f02b2d3049de287cf802fbda8783738d405c21e
OzcarDev <osja1601@gmail.com>
2022-10-26 21:18:54 -0500

    SI

---
+++ ProyectoSemestral2/Assets/_OzcarDev/Scripts/Elevator/Cabin.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cabin : MonoBehaviour
+{
+	
+	public Transform EndPoint,StartPoint;
+	public GameObject cabin;
+	public float speed;
+	private Vector3 currentPoint;
+	private bool isActive;
+	// Start is called on the frame when a script is enabled just before any of the Update methods is called the first time.
+	protected void Start()
+	{
+		isActive=false;
+		currentPoint=StartPoint.position;
+	}
+	
+	public void ActivateElevator()
+	{
+		isActive=true;
+	}
+	
+	
+	// Update is called every frame, if the MonoBehaviour is enabled.
+	protected void Update()
+	{
+		if(!isActive)return;
+		cabin.transform.position = Vector3.MoveTowards(transform.position,currentPoint,speed*Time.deltaTime);
+		if(cabin.transform.position==EndPoint.position){
+			currentPoint=StartPoint.position;
+			isActive=false;
+		}
+		if(cabin.transform.position == StartPoint.position){
+			currentPoint=EndPoint.position;
+			isActive=false;
+		}
+	}
+}
---
+++ ProyectoSemestral2/Assets/_OzcarDev/Scripts/Elevator/Elevator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+	
+	public PlayAnim doors;
+    
+	public void _Elevator()
+	{
+		StartCoroutine(ElevatorCoroutine());
+	}
+	
+	IEnumerator ElevatorCoroutine()
+	{
+		gameObject.GetComponent<BoxCollider>().enabled = false;
+		doors.PlayAnimation();
+		yield return new WaitForSeconds(3);
+		doors.PlayAnimation();
+		gameObject.GetComponent<BoxCollider>().enabled = true;
+	}
+	
+	// OnTriggerEnter is called when the Collider other enters the trigger.
+	protected void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag=="Player"){
+			other.transform.parent = transform;
+		}
+	}
+	
+	// OnTriggerExit is called when the Collider other has stopped touching the trigger.
+	protected void OnTriggerExit(Collider other)
+	{
+		if(other.gameObject.tag=="Player"){
+			other.transform.parent = null;
+		}
+	}
+}
